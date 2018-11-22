52f6d45764740074d2ec3ac4ec56b7533e4ea777
jraspudic <jurica.raspudic@gmail.com>
2018-11-22 01:38:06 +0100

    RESTful routes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var express         = require("express"),
-   app              = express(),
-   bodyParser       = require("body-parser"),
-   mongoose         = require("mongoose");
+var bodyParser       = require("body-parser"),
+   mongoose         = require("mongoose"),
+   expressSanitizer = require("express-sanitizer"),
+   methodOverride   = require("method-override"),
+   express         = require("express"),
+   app              = express();
 
-mongoose.connect("mongodb://localhost/gamingArena");
+mongoose.connect("mongodb://localhost:27017/gamingArena", { useNewUrlParser: true });
 
 app.use(express.static('views'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+
 
 var artiklSchema = new mongoose.Schema({
     naziv: String,
@@ -18,30 +23,13 @@ var artiklSchema = new mongoose.Schema({
 
 var Artikl = mongoose.model("Artikl", artiklSchema);
 
+    //pocetna stranica
 app.get("/", function(req,res){
-   res.render("landing"); 
+   res.render("pocetna"); 
 });
 
-app.post("/shop",function(req,res){
-    var naziv = req.body.naziv;
-    var slika = req.body.slika;
-    var opis = req.body.opis;
-    var cijena = req.body.cijena
-    var noviArtikl = {naziv: naziv, slika: slika, opis: opis, cijena: cijena};
-    Artikl.create(
-    noviArtikl,
-    function(err,artikl){
-        if(err) console.log("error");
-        else    console.log("dodan novi artikl " + artikl);
-    });
-    
-    res.redirect("/shop");
-});
-
-app.get("/shop/new", function(req,res){
-   res.render("new"); 
-});
-
+/***************************** SHOP *************************************/
+    //Index route
 app.get("/shop", function(req,res){
    
     Artikl.find({}, function(err, sviArtikli){
@@ -51,6 +39,79 @@ app.get("/shop", function(req,res){
         else  res.render("shop",{artikli: sviArtikli});
     });
 });
+    //Create route
+app.post("/shop",function(req,res){
+    req.body.artikl.opis = req.sanitize(req.body.artikl.opis); 
+    Artikl.create(
+    req.body.artikl,
+    function(err,noviArtikl){
+        if(err)
+            console.log("error");
+        else   
+            console.log("Dodan novi artikl: " + noviArtikl);
+    });
+    
+    res.redirect("/shop");
+});
+    //New route
+app.get("/shop/new", function(req,res){
+   res.render("dodajArtikl"); 
+});
+
+    //Show route
+app.get("/shop/:id", function(req, res){
+     Artikl.findById(req.params.id, function(err, pronadjenArtikl){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("artikl",{artikl: pronadjenArtikl});
+        }
+    });
+});
+    //edit route
+app.get("/shop/:id/edit", function(req, res) {
+    Artikl.findById(req.params.id, function(err, pronadjenArtikl){
+       if(err){
+            console.log(err);
+            res.redirect("/shop");
+        }
+        else{
+            res.render("editArtikl",{artikl: pronadjenArtikl});
+        }
+    });
+});
+    //update route
+app.put("/shop/:id", function(req,res){
+    req.body.artikl.opis = req.sanitize(req.body.artikl.opis); 
+    Artikl.findByIdAndUpdate(req.params.id,
+    req.body.artikl,
+    function(err,noviArtikl){
+        if(err){
+            console.log("error");
+        }
+        else   
+            console.log("Artikl ažuriran: " + noviArtikl);
+    });
+    
+    res.redirect("/shop");
+});
+
+    //destroy route
+app.delete("/shop/:id",function(req,res){
+    Artikl.findByIdAndDelete(req.params.id,
+    function(err,artikl){
+        if(err){
+            console.log("error");
+        }
+        else   
+            console.log("Artikl pobrisan: " + artikl);
+    });
+    
+    res.redirect("/shop");
+});
+
+/*************************************************************************/
 
 app.get("/novosti", function(req,res){
    res.render("novosti"); 
